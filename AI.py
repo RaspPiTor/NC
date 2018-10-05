e341b08387bbb5af72aaca7924c77a05fb87bb0d
Alex Williams <hughaw64@gmail.com>
2018-10-05 16:57:19 +0100

    Made AI do edges and diagonals

--- AI.py
+++ AI.py
@@ -11,26 +11,29 @@ class semi_AI3():
             middle = [(size ** 2 - size)//2, (size ** 2 - size)//2 - 1]
             middle.extend([middle[0] + size, middle[1] + size])
         corners = [0, size - 1, size * (size - 1), size ** 2 - 1]
-        self.to_do = middle + corners
+        edges = list(range(size)) + list(range(size**2 - size, size ** 2))
+        edges += list(range(0, size ** 2, size))
+        edges += list(range(size - 1, size ** 2 - size, size))
+        diagonals = [size * i + i for i in range(size)]
+        diagonals += [size * i + size - i - 1for i in range(size)]
+        self.to_do = middle + corners + edges + diagonals
         self.to_do.extend(i for i in range(size ** 2) if i not in self.to_do)
         self.size = size
     def get(self, board, player, enemy):
         size = self.size
         to_do = self.to_do
-        for i in range(size ** 2):
-            if i in to_do:
-                b = board.copy()
-                if b.quick_set(i, player):
-                    if b.winner() == player:
-                        to_do.remove(i)
-                        return i // size, i % size
-        for i in range(size ** 2):
-            if i in to_do:
-                b = board.copy()
-                if b.quick_set(i, enemy):
-                    if b.winner() == enemy:
-                        to_do.remove(i)
-                        return i // size, i % size
+        for i in to_do:
+            b = board.copy()
+            if b.quick_set(i, player):
+                if b.winner() == player:
+                    to_do.remove(i)
+                    return i // size, i % size
+        for i in to_do:
+            b = board.copy()
+            if b.quick_set(i, enemy):
+                if b.winner() == enemy:
+                    to_do.remove(i)
+                    return i // size, i % size
         for i in to_do:
             b = board.copy()
             if b.can_set(i):
